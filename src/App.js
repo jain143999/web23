409e5e4001616124f6dfb3c4b3fd4a4b1cd25009
jain143999 <jainshubham52535@gmail.com>
2023-12-23 11:25:47 +0530

    folder structure and read me updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Profile from './components/profile/Profile';
 function App() {
